db26568cbadc7342fbfa968540b381e6afd4c5c0
ervanervan <ervanervan040@gmail.com>
2023-10-08 22:27:10 +0700

    Finished creating the text fitur on the mobile display

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../components/Navbar/NavBar";
 import Hero from "../components/Hero/Hero";
+import Fitur from "../components/Fitur/Fitur";
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <Fitur />
     </>
   );
 };
